1dd1c0cbbbd35181b561ae6e8c7f9b97b998489b
Ishtiaq <ishtiaq.wasi@gmail.com>
2025-12-07 00:43:35 +1100

    project setup

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="py-20 bg-blue-100">
+      <h1>This is footer</h1>
+    </div>
+  );
+};
+
+export default Footer;
